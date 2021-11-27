b35ff7990891d31d391c9b4080801de77baa3d80
Leired7 <leirediez.7@gmail.com>
2021-11-27 18:50:09 +0100

    Test: Demuestra que las configuraciones de test están bien realizadas

--- src/pages/index.test.tsx
+++ src/pages/index.test.tsx
@@ -1,7 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import IndexPage from '.';
+import React from 'react';
+import { render } from '@testing-library/react';
 
-describe('render', () => {
-  render(<IndexPage />);
-  screen.getByText('hola');
+// You have to write data-testid
+const Title = () => <h1 data-testid="hero-title">Gatsby is awesome!</h1>;
+
+test('Displays the correct title', () => {
+  const { getByTestId } = render(<Title />);
+  // Assertion
+  expect(getByTestId('hero-title')).toHaveTextContent('Gatsby is awesome!');
+  // --> Test will pass
 });
